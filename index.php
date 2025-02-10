b1758c1c56833ee40ea6c4a0c56934409a08a52d
luiz <gladnavitorialacerda@gmail.com>
2025-02-10 20:27:22 -0300

    aula10/02/2025

--- index.php
+++ index.php
@@ -8,7 +8,50 @@
 
     <link rel="stylesheet" href="css/styles.css" />
 </head>
+
 <body>
+
+<div class="interface">
+
+    <header class="cabecalho">
+
+        <nav class="menu">
+
+        <div class="logo">
+
+            <img src="imagens/logo.png" alt="LOGO" width="150"/>
+
+        </div>
+        <ul>
+                <li>Home</li>
+                <li>Serviços</li>
+                <li>Produtos</li>
+                <li>Portifólio</li>
+                <li>Sobre</li>
+                <li>Contatos</li>
+        </ul>
+        <ol>
+        </ol>
+        </nav>
+        <div class="titles">
+
+            <h1 class="título">Entregando serviços de qualidade</h1>
+            <h1 class="subtítulo">Soluções em TI</h1>
+
+            <p class="paragrafo">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore doloribus ducimus quam vero provident a?</p>
+        </div>
+        <div class="socialIcons">
+
+            <a href="#">Twitter</a>
+            <a href="#">Facebook</a>
+            <a href="#">Linkedin</a>
+            <a href="#">Instagram</a>
     
+        </div>
+    </header>
+
+</div>
+
 </body>
+
 </html>
