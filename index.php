b5b3790bef4a1d156c42b728b9091bba708c56a4
luiz <gladnavitorialacerda@gmail.com>
2025-03-10 20:45:52 -0300

    aula 10/03/2025

--- index.php
+++ index.php
@@ -1,61 +1,56 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Portifólio</title>
 
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="tools/fontawesome/css/all.css"/>
+
+    <link rel="stylesheet" href="css/styles.css" />
 
 </head>
-
 <body>
 
 <div id="interface">
 
     <header class="cabecalho">
 
-             <nav class="menu">
+        <nav class="menu">
 
-                <div class="logo">
+            <div class="logo">
+                <img src="Imagens/logo.png" alt="LOGO" />
+            </div>
 
-                    <img src="imagens/logo.png" alt="LOGO" width="150"/>
-
-</div>
-
-        <ul>
+            <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Serviços</a></li>
                 <li><a href="#">Produtos</a></li>
                 <li><a href="#">Portifólio</a></li>
                 <li><a href="#">Sobre</a></li>
-                <li><a href="#">Contatos</a></li>
-        </ul>
+                <li><a href="#">Contato</a></li>
+            </ul>
 
         </nav>
+        
+        <div class="titles"> <!-- ========================= -->
+            <div class="container-titles">
+                <h1 class="titulo">Entregando Serviços de Qualidade</h1>
+                <h1 class="subtitulo">Soluções em TI</h1>
 
-        <div class="titles"> <!--========================================-->
-
-            <div class= "container-titles">
-
-                <h1 class="título">Entregando Serviços de Qualidade</h1>
-                <h1 class="subtítulo">Soluções em TI</h1>
-
-                <p class="paragrafo">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore doloribus ducimus quam vero provident a?</p>
-        </div>
+                <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ipsam, earum doloribus id impedit qui!</p>
+            </div>
 
             <div class="socialIcons">
-            <a href="#">Twitter</a>
-            <a href="#">Facebook</a>
-            <a href="#">Linkedin</a>
-            <a href="#">Instagram</a>
-    
-             </div><!--=================================================-->
-
+                <a href="#" title="Visite nosso Twitter"><i class="fab fa-twitter"></i></a>
+                <a href="#" title="Visite nosso Facebook"><i class="fab fa-facebook"></i></a>
+                <a href="#" title="Visite nosso Linkedin"><i class="fab fa-linkedin"></i></a>
+                <a href="#" title="Visite nosso Instagram"><i class="fab fa-instagram"></i></a>
+                </div> <!-- ========================== -->
+             
     </header>
 
 </div>
-
+    
 </body>
-
-</html>
+</html>  
