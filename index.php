a76d5877948aba32364d695e0205a1fc433fb3d2
luiz <gladnavitorialacerda@gmail.com>
2025-03-17 20:28:30 -0300

    aula 17/03/2025

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
     <link rel="stylesheet" href="css/styles.css" />
 
-</head>
+    </head>
 <body>
 
 <div id="interface">
@@ -25,10 +25,10 @@
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Serviços</a></li>
-                <li><a href="#">Produtos</a></li>
                 <li><a href="#">Portifólio</a></li>
                 <li><a href="#">Sobre</a></li>
                 <li><a href="#">Contato</a></li>
+                <li><a href="#">Restrito</a></li>
             </ul>
 
         </nav>
@@ -50,6 +50,77 @@
              
     </header>
 
+    <!--==================================== -->
+
+    <main id="corpo">
+        <section class= "servicos">
+
+            <article class="servico">
+                <img src= "Imagens/service-design.svg" alt="Design" />
+                <h3 class="title-servicle"> Desenvolvimento Web </h3>
+                <p class="text-servico">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, placeat? Ipsam dolorum iure impedit natus.</p>
+            </article>
+            <article class="servico">
+                <img src="Imagens/service-hosting.svg" alt="Hosting">
+                <h3 class="title-servicle"> Web Hosting </h3>
+                <p class="text-servico">Tempora, nisi. Perspiciatis, ab minima! Nisi distinctio explicabo cumque, nemo magnam asperiores doloremque accusamus facilis.</p>
+            </article>
+            <article class="servico">
+                <img src="Imagens/service-social.svg" alt="Social">
+                <h3 class="title-servicle"> Mídias Sociais </h3>
+                <p class="text-servico">Rem quam saepe optio sit molestiae veniam nisi magni odit ex, sed nihil excepturi quia?</p>
+            </article>
+            <article class="servico">
+                <img src="Imagens/service-seo.svg" alt="Seo">
+                <h3 class="title-servicle"> Aplicação SEO </h3>
+                <p class="text-servico">Sint nesciunt sit iure itaque facilis dolor, beatae tempora nihil eius nobis ullam reprehenderit dicta.</p>
+            </article>
+            <article class="servico">
+                <img src="Imagens/service-cloud.svg" alt="Cloud">
+                <h3 class="title-servicle"> Computação em Nuvem </h3>
+                <p class="text-servico">Illo molestias optio eius officia reiciendis, fugiat esse autem dolor expedita itaque, culpa debitis architecto!</p>
+            </article>
+            <article class="service">
+                <img src="Imagens/service-secure.svg" alt="Security">
+                <h3 class="title-servicle"> Segurança de Dados </h3>
+                <p class="text-servico">Placeat nulla saepe architecto minus voluptatem autem quos pariatur fuga illo cum! Ratione, repudiandae sint.</p>
+            </article>
+
+         </section>
+            <section class= "portifolio">
+
+                <h3 class= "title-portifolio"> Nosso Portifólio</h3>
+                <p class= "subtitle"> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+
+                    <div class="container-imagens">
+                        <a href= "#" class="img" style= "background-image: url('imagens/product-1.jpg')">
+                     
+                        </a>
+                        <a href= "#" class="img" style= "background-image: url('imagens/product-2.jpg')">
+                     
+                        </a>
+                        <a href= "#" class="img" style= "background-image: url('imagens/product-3.jpg')">
+                     
+                        </a>
+                        <a href= "#" class="img" style= "background-image: url('imagens/product-4.jpg')">
+                     
+                        </a>
+                        <a href= "#" class="img" style= "background-image: url('imagens/product-5.jpg')">
+                     
+                        </a>
+                        <a href= "#" class="img" style= "background-image: url('imagens/product-6.jpg')">
+                     
+                        </a>
+                       
+                    </div>
+
+            </section>
+
+        </section class= "sobre">
+
+        
+        </section>
+    </main>
 </div>
     
 </body>
